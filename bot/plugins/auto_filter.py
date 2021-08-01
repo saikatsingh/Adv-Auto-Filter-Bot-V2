aaade5885cf6f5bc65089b2585409ba22b007df9
Team @MoviZenX <84813706+saikatsingh@users.noreply.github.com>
2021-08-02 01:46:36 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -147,13 +147,13 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("Next ⏩", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("Next 👉🏼👉🏼", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"⭐ Page 1/{len_result if len_result < max_pages else max_pages} ⭐", callback_data="ignore")
         ])
         
         
