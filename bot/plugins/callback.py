b9140a37e2cb7cab476b190b4b38559562cd0a1d
Team @MoviZenX <84813706+saikatsingh@users.noreply.github.com>
2021-08-31 17:51:48 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1600,8 +1600,8 @@ async def cb_about(bot, update: CallbackQuery):
 
     text=f"<i><u>Bot's Status</u></i>\n"
     text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
-    text+=f"\n<b><i>Bot Funtion:</i></b> <i>Mia Malkova</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/movizenix">@movizenix</a>\n"""
+    text+=f"\n<b><i>Bot Funtion:</i></b> <i>TheHarleyQuinnBot</i>\n"
+    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/mvznx">@movizenix</a>\n"""
     text+="""\n<b><i>About Dev:</i></b> <a href="https://t.me/AnnihilusOP">Saikat Singh</a>"""
 
     buttons = [
